00646410b4b788366cce8959e4b0e2b3a116b7a3
JPC1206 <144404479+JPC1206@users.noreply.github.com>
2024-10-24 13:20:05 -0700

    removed firebase mention

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart'; // basic flutter package
 import 'pages/home_page.dart'; // Import HomePage
 
-// ignore: depend_on_referenced_packages
-import 'package:firebase_core/firebase_core.dart';
-
 void main() async {
-  //
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // checks if platform is web or not, either Android or Apple
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-        options: FirebaseOptions(
-            apiKey: "AIzaSyDJryKyP4A4nmEIPLSNB_prakcH1syqpzw",
-            authDomain: "booklite-bfbdf.firebaseapp.com",
-            databaseURL: "https://booklite-bfbdf-default-rtdb.firebaseio.com",
-            projectId: "booklite-bfbdf",
-            storageBucket: "booklite-bfbdf.appspot.com",
-            messagingSenderId: "954069129873",
-            appId: "1:954069129873:web:4bf6289429fb77c3c53d30",
-            measurementId: "G-ZYRYWQCQWV"));
-  } else {
-    await Firebase.initializeApp();
-  }
-
   runApp(const StyleHiveApp());
 }
 
