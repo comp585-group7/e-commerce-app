275d8f6e978381b247ae21c4b22b667705adeb28
JP1206 <131424995+JP1206@users.noreply.github.com>
2024-12-10 22:43:10 -0800

    pushing latest changes
    Not have fixed issue "Payment Cancelled"

--- android/app/src/main/kotlin/com/example/ecommerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/ecommerce/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.ecommerce
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterFragmentActivity() {
+    // If you're using onCreate/onPostResume etc, ensure they're compatible.
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
